399b7bdab2f937d97d97d024445aecbedc4a6a6e
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-07-01 10:36:53 +0800

    Initial commit

---
+++ demo/src/main/resources/CapStone.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS CarDealership;
+CREATE DATABASE CarDealership;
+
+USE CarDealership;
+
+DROP TABLE IF EXISTS Admin;
+
+
+DROP TABLE IF EXISTS Vehicle;
+CREATE TABLE Vehicle(
+    vehicle_id INT PRIMARY KEY AUTO_INCREMENT,
+    vehicle_model_name VARCHAR(255) NOT NULL,
+    'year' DATE NOT NULL,
+    make ENUM('Make') NOT NULL,
+    mmodel ENUM('
+     
+);
+
+CREATE TABLE User(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    'user_name' VARCHAR(255) NOT NULL,
+    account_type ENUM('Disabled','Sales','Admin') NOT NULL,
+    'password' VARCHAR(255) NOT NULL,
+   
+);
+
+DROP TABLE IF EXISTS Order;
+CREATE TABLE Order(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+  
+);
