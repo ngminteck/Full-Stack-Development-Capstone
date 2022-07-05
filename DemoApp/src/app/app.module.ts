76737f0c25c8f71e7e51561a25c51062c51d82b3
Debs <lapet1558@gmail.com>
2022-07-05 16:54:17 +0800

    WIP Angular Backend

---
+++ DemoApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddPostComponent,
+    PostDetailsComponent,
+    PostsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
