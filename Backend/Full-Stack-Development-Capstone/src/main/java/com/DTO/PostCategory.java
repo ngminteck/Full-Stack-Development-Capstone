ffc8c680ab811e3a24a4364dbefd645dcbfe3683
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-07-07 13:02:44 +0800

    fix post id and cat id composite id compile error
    still have 1more error for user drop table errror

---
+++ Backend/Full-Stack-Development-Capstone/src/main/java/com/DTO/PostCategory.java
@@ -0,0 +1,64 @@
+package com.DTO;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="PostCategory")
+@IdClass(CompositeCatIDAndPostID.class)
+public class PostCategory {
+
+	@Column(name = "post_id")
+	private @Id Long postId;
+	
+	@Column(name = "category_id")
+	private @Id Long categoryId;
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	@Override
+	public String toString() {
+		return "PostCategory [postId=" + postId + ", categoryId=" + categoryId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoryId, postId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PostCategory other = (PostCategory) obj;
+		return Objects.equals(categoryId, other.categoryId) && Objects.equals(postId, other.postId);
+	}
+	
+	
+}
---
+++ Backend/Full-Stack-Development-Capstone/src/main/java/com/DTO/User.java
@@ -0,0 +1,81 @@
+package com.DTO;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+
+	@Column(name = "user_id")
+    private @Id @GeneratedValue Long userID;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "is_admin")
+	private boolean isAdmin;
+
+	public Long getUserID() {
+		return userID;
+	}
+
+	public void setUserID(Long userID) {
+		this.userID = userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean admin) {
+		isAdmin = admin;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", email=" + email + ", password=" + password + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(userID, other.userID);
+	}
+	
+	
+	
+}
