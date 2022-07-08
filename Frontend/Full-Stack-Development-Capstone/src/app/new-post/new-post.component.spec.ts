14616efa6e303756f66ed97d2dc12f8644e80a7c
ClarenceNgMinTeck <ng_min_teck@hotmail.com>
2022-07-08 11:51:22 +0800

    update tinymce
    update tinymce

---
+++ Frontend/Full-Stack-Development-Capstone/src/app/new-post/new-post.component.spec.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { AppComponent } from '../app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    EditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
